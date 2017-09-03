e616b222b03183e16c7e33ec939121e2e7edcc05
Lorna Jane Mitchell <lorna@lornajane.net>
2017-09-03 20:05:02 +0000

    Add the version for the sense hat

---
+++ temperature_to_mqtt/temperature_sense_hat.py
@@ -0,0 +1,22 @@
+import paho.mqtt.client as mqtt
+from sense_hat import SenseHat
+import socket
+import time
+
+sensor = SenseHat()
+broker_address="10.1.0.1" 
+
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+
+client = mqtt.Client("P1") #create new instance
+client.connect(broker_address) #connect to broker
+ip = get_ip_address() # get current IP so we can ID ourselves
+
+while True:
+	temperature_in_celsius = sensor.get_temperature()
+	# print("Publishing message to topic","/house/temperature")
+	client.publish("/house/temperature",'{"temperature": {"value": ' + str(temperature_in_celsius) + ', "units": "C"}, "from_ip": ' + str(ip) + '}')
+	time.sleep(5);
