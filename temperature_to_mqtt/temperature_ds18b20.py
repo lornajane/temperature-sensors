e76c86b334bad74d7fac426af193d6c71549deef
Lorna Jane Mitchell <lorna@lornajane.net>
2017-10-01 22:00:30 +0100

    Move to multiple temperature sensor support, use device ID instead of IP address

--- temperature_to_mqtt/temperature_ds18b20.py
+++ temperature_to_mqtt/temperature_ds18b20.py
@@ -3,21 +3,16 @@ from w1thermsensor import W1ThermSensor
 import socket
 import time
 
-sensor = W1ThermSensor()
-broker_address="10.1.0.1" 
-
-def get_ip_address():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    return s.getsockname()[0]
+broker_address="10.1.0.1"
 
 client = mqtt.Client("P1") #create new instance
 client.connect(broker_address) #connect to broker
-ip = get_ip_address() # get current IP so we can ID ourselves
 
 while True:
-	temperature_in_celsius = sensor.get_temperature()
-	# print("Publishing message to topic","/house/temperature")
-	client.publish("/house/temperature",'{"temperature": {"value": ' + str(temperature_in_celsius) + ', "units": "C"}, "from_ip": ' + str(ip) + '}')
-	time.sleep(5);
+    for sensor in W1ThermSensor.get_available_sensors():
+        temperature_in_celsius = sensor.get_temperature()
+        # print("Publishing message to topic","/house/temperature")
+        client.publish("/house/temperature",'{"temperature": {"value": ' + str(temperature_in_celsius) + ', "units": "C"}, "sensor_id": ' + sensor.id + '}')
+
+    time.sleep(5);
 
