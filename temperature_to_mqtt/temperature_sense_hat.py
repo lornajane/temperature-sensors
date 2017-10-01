69f34764c1b1640f3d09e1286bfa0a158f343264
Lorna Jane Mitchell <lorna@lornajane.net>
2017-10-01 21:33:39 +0000

    Update sensehat code to use sensor ID (warning: hardcoded)

--- temperature_to_mqtt/temperature_sense_hat.py
+++ temperature_to_mqtt/temperature_sense_hat.py
@@ -7,18 +7,12 @@ import uuid
 sensor = SenseHat()
 broker_address="10.1.0.1" 
 
-def get_ip_address():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    return s.getsockname()[0]
-
-client_name = uuid.uuid4()
-client = mqtt.Client(str(client_name)) #create new instance
+client = mqtt.Client() #create new instance
 client.connect(broker_address) #connect to broker
-ip = get_ip_address() # get current IP so we can ID ourselves
 
 while True:
 	temperature_in_celsius = sensor.get_temperature()
 	# print("Publishing message to topic","/house/temperature")
-	client.publish("/house/temperature",'{"temperature": {"value": ' + str(temperature_in_celsius) + ', "units": "C"}, "from_ip": "' + str(ip) + '"}')
+	# beware hardcoded identifier if you use more than one SenseHAT!
+	client.publish("/house/temperature",'{"temperature": {"value": ' + str(temperature_in_celsius) + ', "units": "C"}, "sensor_id": "SenseHAT"}')
 	time.sleep(5);
